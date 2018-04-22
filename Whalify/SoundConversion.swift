18f7cee70f3b58f8d79fdd1628d96aa272d83b69
James VB <james@myquidd.com>
2018-04-22 11:37:38 -0400

    initial

---
+++ Whalify/SoundConversion.swift
@@ -0,0 +1,16 @@
+//
+//  SoundConversion.swift
+//  Whalify
+//
+//  Created by James VanBeverhoudt on 4/22/18.
+//  Copyright © 2018 James VanBeverhoudt. All rights reserved.
+//
+
+import Foundation
+
+public extension Double {
+
+    public func divideBy(value: Double) -> Double {
+        return self / value
+    }
+}
