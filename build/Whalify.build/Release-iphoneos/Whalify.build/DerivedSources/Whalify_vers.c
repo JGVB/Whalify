63fa2de891dc44c8532b58c86552142d8529fcd3
James VB <james@myquidd.com>
2018-04-22 11:45:24 -0400

    travis yaml

---
+++ build/Whalify.build/Release-iphoneos/Whalify.build/DerivedSources/Whalify_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char WhalifyVersionString[];
+ extern const double WhalifyVersionNumber;
+
+ const unsigned char WhalifyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Whalify  PROJECT:Whalify-1" "\n";
+ const double WhalifyVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/Whalify.build/Release-iphoneos/Whalify.build/Objects-normal/arm64/Whalify_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jamesvanbeverhoudt/GoogleDrive/iOSApps/Whalify/build/Whalify.build/Release-iphoneos/Whalify.build/DerivedSources/Whalify_vers.c
