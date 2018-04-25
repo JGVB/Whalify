adf102c7aaad2677f09aaf73c7ba8f3838e8fd49
James VB <james@myquidd.com>
2018-04-24 21:25:26 -0400

    revert

--- Whalify/SoundConversion.swift
+++ Whalify/SoundConversion.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import AVFoundation
+
+public class Whalify {
+
+    private static var player: AVAudioPlayer?
+    private static var player1: AVPlayer?
+
+    public static func changePitchOf(sound url: URL) {
+        let asset = AVAsset(url: url)
+        let playerItem = AVPlayerItem(asset: asset)
+        player1 = AVPlayer(playerItem: playerItem)
+        player1?.play()
+https://www.raywenderlich.com/145770/audiokit-tutorial-getting-started
+//        do {
+//            player = try AVAudioPlayer(contentsOf: url)
+//
+//            DispatchQueue.global().async {
+//                player?.prepareToPlay()
+//                player?.play()
+//            }
+//        } catch {
+//            return
+//        }
+    }
+}
